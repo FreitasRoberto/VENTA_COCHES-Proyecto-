27646119b2391f51d911aa7cb5545d58a7b0ff16
Roberto Freitas <robertofreitas.dev@gmail.com>
2023-03-08 13:57:28 +0100

    Add files via upload

---
+++ js/db.js
@@ -0,0 +1,174 @@
+const autos = [
+	{
+		marca: 'Renault',
+		modelo: 'AUSTRAL E-TECH FULL HYBRID',
+		year: 2022,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Volkswagen', 
+        modelo: 'T-ROC', 
+        year: 2020, 
+        precio: 30000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'Mazda',
+		modelo: 'CX-60',
+		year: 2022,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'manual'
+	},
+	{ 
+        marca: 'Seat', 
+        modelo: 'Tarraco', 
+        year: 2023, 
+        precio: 40000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2016,
+		precio: 70000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Fiat',
+		modelo: 'Híbrido',
+		year: 2020,
+		precio: 25000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Seat',
+		modelo: 'Polo',
+		year: 2020,
+		precio: 15000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Ranger', 
+        year: 2022, 
+        precio: 80000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Renault',
+		modelo: 'Megane',
+		year: 2018,
+		precio: 20000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Jeep', 
+        modelo: 'Avenger', 
+        year: 2023, 
+        precio: 55000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 
+        'automatico' 
+    },
+	{
+		marca: 'Renault',
+		modelo: 'Captur',
+		year: 2020,
+		precio: 25000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{
+		marca: 'Volkswagen',
+		modelo: 'Arona',
+		year: 2018,
+		precio: 25000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2019,
+		precio: 90000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Mondeo', 
+        year: 2017, 
+        precio: 20000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2015,
+		precio: 35000,
+		puertas: 2,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2018,
+		precio: 50000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie x6',
+		year: 2017,
+		precio: 40000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A4', 
+        year: 2016, 
+        precio: 30000, 
+        puertas: 4, 
+        color: 'Azul', 
+        transmision: 'automatico' 
+    }
+];
